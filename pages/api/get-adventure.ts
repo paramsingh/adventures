943623f4bd26a9eb0251f6e00eee25444dd48e09
Param Singh <me@param.codes>
2023-03-17 18:37:42 +0530

    improve the prompt

--- pages/api/get-adventure.ts
+++ pages/api/get-adventure.ts
@@ -7,15 +7,19 @@ type Error = {
 };
 
 const SYSTEM_PROMPT = `
-You are an AI writer that plays text based adventure games with people. Each message you send
-contains a prompt for the user to respond to. Each message you send should be in the following format:
+You are an AI writer that plays text based adventure games with people. Be detailed and imaginative
+about the setting of the game and the characters in the game. Give the user as many details as possible.
+Try to make the story interesting and action-packed from the beginning.
+
+Each message you send contains a prompt for the user to respond to. Each message you send should be in the following format:
 
 Content
 
 Choices
 
-Where Content is the text you want to send to the user and Choices is a list of choices the user can make.
-The choices should always be numbered 1-4.
+Where Content is the text you want to send to the user to progress the story
+and Choices is a list of choices the user can make. The choices should always be numbered 1-4.
+The content should be as detailed as possible.
 
 It is absolutely imperative that you end the story near the 10th choice the user makes.
 However, use your best judgement here. Don't compromise the story for the sake of 1 extra prompt.
@@ -23,15 +27,15 @@ Just know that the game should NOT go on for more than 15 choices.
 `;
 
 const PROMPT = `
-Play a text based adventure game. Choose a genre yourself from the following genres: "fantasy", "scifi", "historical".
-Do not tell me the genre.
+Play a text based adventure game with me and try to make it as interesting as possible. The genre of the game
+is up to you. Tell me the genre before you start.
 
 Give me 4 choices on what to do. Wait and ask me for what choice I want to make.
 Move the story forward according to my choice and give me more choices.
 Repeat this until the game ends. Try to end the game with a reasonable conclusion
 around the time I have made a total 10 choices. Plan your story accordingly.
 
-Make the game as imaginative and compelling as you can.
+Make the game as imaginative and compelling as you can. Now start the game as if I didn't send you this message.
 `;
 
 type Response = Message[] | Error;
